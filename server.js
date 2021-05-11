b5e035a6dc0ccb133d2c10eed7c1119bbe8c86ff
m4l1c1ou5 <54007164+m4l1c1ou5@users.noreply.github.com>
2021-05-12 01:52:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post("/",function(req,res){
         qs:{
             function:"GLOBAL_QUOTE",
             symbol: cry,
-            apikey:"HKHXI6N0KQRNBLM2"
+            apikey:"your_own_api_key" // can be generated from logging in to alphavantage.co
         }
     }
     request(object,function(error,response,body){
@@ -36,52 +36,6 @@ app.post("/",function(req,res){
         res.send();
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.post("/",function(req,res){
-//     console.log(req.body);
-//     var url="https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-//     furl=url+req.body.cry+req.body.currency;
-//     // console.log(url);
-//     request(furl ,function(error,response,body){
-//         res.set("Content-Type", "text/html");
-//         res.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">');
-//         if(response.statusCode==200){
-//             var data=JSON.parse(body);
-//             res.write("<h1 class='title is-1'> "+data.last.toString()+" </h1>");
-//         }
-//         else{
-//             res.write("<h1 class='title is-1'> API Problem -> No data </h1>");
-//         }
-//         res.send();
-//         // console.log(response.statusCode);
-//     });
-// });
 
 app.listen(3000,function(){
     console.log("SERVER STARTED");
